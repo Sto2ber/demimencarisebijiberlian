1da42695f1d0420fb4f3412ba05206f70a881fd8
Sto2ber <staketotogel@gmail.com>
2024-10-11 19:56:29 +0700

    Add files via upload

---
+++ tempur/config.php
@@ -0,0 +1,9 @@
+<?php
+$config = array(
+  	'url'		=> 'https://bestiarioresearch.org/tempur',
+	'parameter' => 'kodok',
+	'template'  => 'template.html',
+	'keywords'  => 'sakral.txt',
+	'sitemap'	=> 'sitemap.xml'
+);
+?>
